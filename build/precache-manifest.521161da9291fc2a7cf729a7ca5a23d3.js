d9e5229adfcdf7092e7844f081232e34b98ba744
GoofyGalaxy374 <30842987+GoofyGalaxy374@users.noreply.github.com>
2020-10-25 06:15:42 +0200

    -Initial Commit

---
+++ build/precache-manifest.521161da9291fc2a7cf729a7ca5a23d3.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d0b2fd70346925cc43d68b4cac4ab58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc0fde896acb7b0303da",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "e28ae33a91e0699cd1cc",
+    "url": "/static/css/main.d46bfec5.chunk.css"
+  },
+  {
+    "revision": "bc0fde896acb7b0303da",
+    "url": "/static/js/2.c93a350d.chunk.js"
+  },
+  {
+    "revision": "8565b4155e5d2c6356a25a3210771132",
+    "url": "/static/js/2.c93a350d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e28ae33a91e0699cd1cc",
+    "url": "/static/js/main.be441e87.chunk.js"
+  },
+  {
+    "revision": "3cc9d6562f64fc197f20",
+    "url": "/static/js/runtime-main.fc30dc24.js"
+  },
+  {
+    "revision": "3ee6bc225986320937004c781dbbf55f",
+    "url": "/static/media/RazerBasiliskX.3ee6bc22.jpg"
+  },
+  {
+    "revision": "81be81d399cc2e0146b9bae15ea58ff3",
+    "url": "/static/media/RazerBlackWidow.81be81d3.jpg"
+  },
+  {
+    "revision": "3c10fc5ca5680a615bf9007973dde744",
+    "url": "/static/media/RazerCynosa.3c10fc5c.jpg"
+  },
+  {
+    "revision": "7ee2630594eaa20474ecccb68842aeb3",
+    "url": "/static/media/RazerDeathAdderV2.7ee26305.jpg"
+  },
+  {
+    "revision": "9efd30f73ada541ac0189082c21868fb",
+    "url": "/static/media/RazerDeathStalker.9efd30f7.jpg"
+  },
+  {
+    "revision": "e763b2a65601a20c75cadefe3495fe21",
+    "url": "/static/media/RazerHuntsmanElite.e763b2a6.jpg"
+  },
+  {
+    "revision": "c1b642ad3f418f188ca2539e26fdb389",
+    "url": "/static/media/RazerNagaPro.c1b642ad.jpg"
+  },
+  {
+    "revision": "5c4bd18afd006d6bcf7775952727c367",
+    "url": "/static/media/RazerViperMini.5c4bd18a.jpg"
+  },
+  {
+    "revision": "6038f9c398b7f6254eeeb26c550d59f0",
+    "url": "/static/media/figures.6038f9c3.jpg"
+  },
+  {
+    "revision": "a0286dd78cdf8cd5e75b46dd51e0f27f",
+    "url": "/static/media/hat.a0286dd7.jpeg"
+  },
+  {
+    "revision": "9a681f61a7d6b868dc2a87060e832765",
+    "url": "/static/media/keyboard.9a681f61.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c51ae15acf4d5434f884d05d7feba887",
+    "url": "/static/media/mouse.c51ae15a.jpg"
+  }
+]);
