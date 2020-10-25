4aa59f597b713ab9401f07b85fc525493b570551
GoofyGalaxy374 <30842987+GoofyGalaxy374@users.noreply.github.com>
2020-10-25 11:04:39 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import UserContainer from './Components/UserContainer';
+import Index from './Components/index'
+import {PersistGate} from 'redux-persist/integration/react'
+import {store} from './Components/store'
 import {Provider} from 'react-redux'
-import store from './Components/store'
+import {persistor} from './Components/store'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Cart from './Components/cart'
+import keyboards from './Components/keyboards'
+import mouses from './Components/mouses'
+import favorites from './Components/favorites'
 function App() {
-
   return (
-    <div>
+    <div className="App">
       <Provider store={store}>
-        <UserContainer></UserContainer>
+        <PersistGate persistor ={persistor}> 
+        <Router>
+          <Switch>
+              <Route exact path='/Home' component={Index}></Route>
+              <Route path='/cart' component={Cart}></Route>
+              <Route path='/keyboards' component={keyboards}></Route>
+              <Route path='/mouses' component={mouses}></Route>
+              <Route path='/wishList' component={favorites}></Route>
 
+
+          </Switch>     
+        </Router>
+        </PersistGate>
       </Provider>
+
     </div>
   );
 }
